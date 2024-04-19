4f5fa0946bb25b371503eff09886e2b5df9a4447
hemanthindala <hemanthindala2001@gmail.com>
2024-04-18 22:10:28 -0400

    Adding Practicun 13 files

--- src/main/java/edu/se/primesservice/controller/PrimesController.java
+++ src/main/java/edu/se/primesservice/controller/PrimesController.java
@@ -2,7 +2,10 @@ package edu.se.primesservice.controller;
 
 
 import ch.qos.logback.classic.spi.ConfiguratorRank;
+import edu.se.primesservice.rabbitmq.MQSender;
 import edu.se.primesservice.service.IPrimesService;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.oauth2.jwt.Jwt;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
@@ -12,11 +15,21 @@ public class PrimesController {
 
     IPrimesService iPrimesService;
 
-    public PrimesController(IPrimesService iPrimesService){
+    private final MQSender mqSender;
+
+
+    public PrimesController(IPrimesService iPrimesService, MQSender mqSender){
         this.iPrimesService = iPrimesService;
+        this.mqSender = mqSender;
     }
     @GetMapping("/{n}")
     public boolean isPrime(@PathVariable int n){
-        return iPrimesService.isPrime(n);
+        boolean result = iPrimesService.isPrime(n);
+        Object principal = SecurityContextHolder
+                .getContext().getAuthentication().getPrincipal();
+        String username = ((Jwt) principal).getSubject();
+        System.out.println(username);
+        mqSender.sendMessage(username, n,result);
+        return result;
     }
 }
